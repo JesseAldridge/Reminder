8feec00380d0e1a9ec70f67a5bb6454fe3f13d77
Jesse Aldridge <jessealdridge@gmail.com>
2019-03-30 13:54:53 -0700

    only run once

--- reminder.py
+++ reminder.py
@@ -1,5 +1,13 @@
-import time
+import time, os, sys, subprocess
 
-while True:
-  print "don't fuck up"
-  time.sleep(60 * 60)
+meta_path = os.path.expanduser('~/reminder.txt')
+
+if os.path.exists(meta_path):
+  sys.exit()
+else:
+  with open(meta_path, 'w') as f:
+    pass
+
+  while True:
+    print "don't fuck up"
+    time.sleep(60 * 60)
