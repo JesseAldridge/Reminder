66c9382f6bbca9cae177c740251463f629990215
Jesse Aldridge <jessealdridge@gmail.com>
2019-03-30 15:24:03 -0700

    keep it simple

--- reminder.py
+++ reminder.py
@@ -1,16 +1,5 @@
 import time, os, sys
 
-meta_path = os.path.expanduser('~/reminder.txt')
-
-if os.path.exists(meta_path):
-  sys.exit()
-else:
-  with open(meta_path, 'w') as f:
-    pass
-
-  try:
-    while True:
-      print "don't fuck up"
-      time.sleep(60 * 60)
-  finally:
-    os.remove(meta_path)
+while True:
+  print "don't fuck up"
+  time.sleep(60 * 60)
